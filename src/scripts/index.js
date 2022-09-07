1d7ad6687b82849ef8957521e8eb8fb5ebf36b7e
arialghifari <arialghifari99@gmail.com>
2022-09-07 23:11:39 +0800

    Add header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import '../styles/main.scss';
 
 console.log('Hello Coders! :)');
--- webpack.common.js
+++ webpack.common.js
@@ -4,39 +4,41 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js'),
-    // sw: path.resolve(__dirname, 'src/scripts/sw.js'),
+    app: path.resolve(__dirname, './src/scripts/index.js'),
+    // sw: path.resolve(__dirname, './src/scripts/sw.js'),
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/templates/index.html'),
       filename: 'index.html',
-      template: path.resolve(__dirname, 'src/templates/index.html'),
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          from: path.resolve(__dirname, './src/public/'),
+          to: path.resolve(__dirname, './dist/'),
         },
       ],
     }),

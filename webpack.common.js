10f83d162795cf297725c4a2b380d783ab0f0832
arialghifari <arialghifari99@gmail.com>
2022-09-09 15:23:44 +0800

    Add aria-label to hamburger-menu

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -43,14 +42,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/templates/index.html'),
       filename: 'index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, './src/public/'),
-          to: path.resolve(__dirname, './dist/'),
-        },
-      ],
     }),
   ],
 };

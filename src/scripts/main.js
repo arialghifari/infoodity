3f3d5f5e9cc2021dc87555a92acd6d56140b421c
arialghifari <arialghifari99@gmail.com>
2022-09-09 13:34:50 +0800

    Responsive and navigation drawer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,16 @@
 const main = () => {
-  // const createExploreCard = () => {
-  //   const exploreListEl = document.querySelector('.explore__list');
-  //   data.restaurants.forEach((restaurant) => {
-  //     const exploreCard = document.createElement('section');
-  //     exploreCard.classList.add('explore__card');
-  //     exploreCard.innerHTML = `
-  // 			<img
-  // 				src="${restaurant.pictureId}"
-  // 				alt="${restaurant.name} image"
-  // 			/>
-  // 			<section class="explore__card__info">
-  // 				<section>
-  // 					<img src="../public/ic-map-pin.svg" alt="Map pin icon" />
-  // 					<p>Medan</p>
-  // 				</section>
-  // 				<section>
-  // 					<img src="../public/ic-star.svg" alt="Star icon" />
-  // 					<p>4.6</p>
-  // 				</section>
-  // 			</section>
-  // 			<section class="explore__card__body">
-  // 				<h3>Bring Your Phone Cafe</h3>
-  // 				<p>
-  // 					But I must explain to you how all this mistaken idea of
-  // 					denouncing pleasure and praising pain was born and I will give
-  // 					you a complete account of the system...
-  // 				</p>
-  // 				<button>
-  // 					<p>View More</p>
-  // 					<img src="../public/ic-arrow-up-right.svg" alt="Arrow icon" />
-  // 				</button>
-  // 			</section>
-  // 		`;
-  //     exploreListEl.append(exploreCard);
-  //   });
-  // };
-  // createExploreCard();
+  const btnHamburger = document.querySelector('.hamburger');
+  const drawer = document.querySelector('.drawer');
+
+  btnHamburger.addEventListener('click', (e) => {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+  });
+
+  document.body.addEventListener('click', (e) => {
+    drawer.classList.remove('open');
+    e.stopPropagation();
+  });
 };
 
 export default main;
